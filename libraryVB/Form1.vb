2cb5752b5487d57891550744e71829098327ad52
DovgaNik <nikita.dovgan.05@hotmail.com>
2023-03-28 13:56:30 +0300

    Added open database button

--- libraryVB/Form1.vb
+++ libraryVB/Form1.vb
@@ -1,6 +1,17 @@
-﻿Imports System.Windows.Forms.VisualStyles.VisualStyleElement
+﻿Imports System.IO
+Imports System.Windows.Forms.VisualStyles.VisualStyleElement
 
 Public Class Form1
+    Function clearDB()
+        DataGridView1.Columns.Clear()
+        DataGridView1.Columns.Add("BookID", "BookID")
+        DataGridView1.Columns.Add("Name", "Name")
+        DataGridView1.Columns.Add("Author", "Author")
+        DataGridView1.Columns.Add("Is Borrowed", "Is borrowed")
+        DataGridView1.Columns.Add("Borrow date", "Borrow date")
+        DataGridView1.Columns.Add("Return date", "Return date")
+    End Function
+
     Private Sub InShaAllahToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles InShaAllahToolStripMenuItem.Click
         FormHelp.Show()
     End Sub
@@ -28,30 +39,44 @@ Public Class Form1
     End Sub
 
     Private Sub CreateANewDatabaseToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles CreateANewDatabaseToolStripMenuItem.Click
-        DataGridView1.Columns.Clear()
-        DataGridView1.Columns.Add("BookID", "BookID")
-        DataGridView1.Columns.Add("Name", "Name")
-        DataGridView1.Columns.Add("Author", "Author")
-        DataGridView1.Columns.Add("Is Borrowed", "Is borrowed")
-        DataGridView1.Columns.Add("Borrow date", "Borrow date")
-        DataGridView1.Columns.Add("Return date", "Return date")
+        clearDB()
     End Sub
 
     Private Sub OpenToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles OpenToolStripMenuItem.Click
-        Dim fileName
+        Dim fileName As String
         Dim opDg As New OpenFileDialog
 
-        opDg.Filter = "text files|*.txt|documents|*.doc;*.docx;*.pdf|all files|*.*"
+        opDg.Filter = "Book database file|*.bookdb|All files|*.*"
         opDg.FilterIndex = 1
-        opDg.DefaultExt = ".txt"
+        opDg.DefaultExt = ".bookdb"
         opDg.AddExtension = True
-        opDg.InitialDirectory = "D:\"
 
         If opDg.ShowDialog() = DialogResult.OK Then
             fileName = opDg.FileName
-            MessageBox.Show(fileName)
         Else
-            MessageBox.Show("No")
+            Exit Sub
         End If
+
+        If File.Exists(fileName) Then
+
+            clearDB()
+            Dim objReader As New StreamReader(fileName)
+            Dim ID As Integer
+            ID = 0
+            Do While objReader.Peek() <> -1
+
+                Dim objects As String()
+                objects = objReader.ReadLine().Split("|".ToCharArray)
+                If objects.Count = 5 Then
+                    DataGridView1.Rows.Add(ID, objects(0), objects(1), objects(2), objects(3), objects(4))
+                End If
+                ID += 1
+            Loop
+
+
+        Else
+            MessageBox.Show("File " + fileName + " does not exist, plese check again!!!")
+        End If
+
     End Sub
 End Class
